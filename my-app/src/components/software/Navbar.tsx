dbb3a2dbdd0fec09f26155a9024fb14f8f3fe65c
Khoa <dangkhoanguyen0812@gmail.com>
2025-03-26 14:55:19 +0700

    add Navbar

--- my-app/src/components/software/Navbar.tsx
+++ my-app/src/components/software/Navbar.tsx
@@ -1,88 +1,39 @@
-import { useState } from "react";
-
 const Navbar = () => {
-  const [open, setOpen] = useState(false);
-
   return (
-    <header className={`flex w-full items-center bg-white mx-auto     `}>
-      <div className="container">
-        <div className="relative -mx-4 flex items-center justify-between">
-          <div className="w-60 max-w-full px-4">
-            <a href="/#" className="block w-full py-5">
-              <img
-                src="https://cdn.tailgrids.com/2.0/image/assets/images/logo/logo-primary.svg"
-                alt="logo"
-                className="dark:hidden"
-              />
-              <img
-                src="https://cdn.tailgrids.com/2.0/image/assets/images/logo/logo-white.svg"
-                alt="logo"
-                className="hidden "
-              />
-            </a>
-          </div>
-          <div className="flex w-full items-center justify-between px-4">
-            <div>
-              <button
-                onClick={() => setOpen(!open)}
-                id="navbarToggler"
-                className={` ${
-                  open && "navbarTogglerActive"
-                } absolute right-4 top-1/2 block -translate-y-1/2 rounded-lg px-3 py-[6px] ring-primary focus:ring-2 lg:hidden`}
-              >
-                <span className="relative my-[6px] block h-[2px] w-[30px] bg-body-color "></span>
-                <span className="relative my-[6px] block h-[2px] w-[30px] bg-body-color "></span>
-                <span className="relative my-[6px] block h-[2px] w-[30px] bg-body-color "></span>
-              </button>
-              <nav
-                id="navbarCollapse"
-                className={`absolute right-4 top-full w-full max-w-[250px] rounded-lg bg-white px-6 py-5 shadow lg:static lg:block lg:w-full lg:max-w-full lg:shadow-none lg:dark:bg-transparent ${
-                  !open && "hidden"
-                } `}
-              >
-                <ul className="block lg:flex">
-                  <ListItem NavLink="/#">Home</ListItem>
-                  <ListItem NavLink="/#">Payment</ListItem>
-                  <ListItem NavLink="/#">About</ListItem>
-                  <ListItem NavLink="/#">Blog</ListItem>
-                </ul>
-              </nav>
-            </div>
-            <div className="hidden justify-end pr-16 sm:flex lg:pr-0">
-              <a
-                href="/#"
-                className="px-7 py-3 text-base font-medium text-dark hover:text-primary "
-              >
-                Sign in
-              </a>
-
-              <a
-                href="/#"
-                className="rounded-md bg-primary px-7 py-3 text-base font-medium text-dark hover:bg-primary/90"
-              >
-                Download
-              </a>
-            </div>
-          </div>
+    <div className="flex justify-between items-center px-[60px] py-[48px] bg-slate-800">
+      <img src="/logo_real_estate.svg" alt="logo" />
+      <ul className="flex text-white gap-[40px] text-sm">
+        <li>Home</li>
+        <li>Listing</li>
+        <li>Member</li>
+        <li>Blog</li>
+        <li>Contact</li>
+      </ul>
+      <div className="flex gap-[20px]">
+        <div className="flex items-center text-white gap-[12px]">
+          <img
+            src="/real_estate/contact.svg"
+            alt="contact_icon"
+            width={20}
+            height={20}
+          />
+          <p className="text-sm">+68 685 88666</p>
         </div>
+        <div className="rounded-full border-[1px] h-10 w-10 flex justify-center items-center">
+          <img
+            src="/real_estate/person.svg"
+            alt="person_icon"
+            className="text-center"
+            height={16}
+            width={16}
+          />
+        </div>
+        <button className="px-6 py-1 border-[1px] rounded-[50px] text-white text-sm">
+          Add Property
+        </button>
       </div>
-    </header>
+    </div>
   );
 };
 
 export default Navbar;
-
-const ListItem = ({ children, NavLink }) => {
-  return (
-    <>
-      <li>
-        <a
-          href={NavLink}
-          className="flex py-2 text-base font-medium text-body-color hover:text-dark lg:ml-12 lg:inline-flex"
-        >
-          {children}
-        </a>
-      </li>
-    </>
-  );
-};
