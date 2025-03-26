4511f43a92d6c7a95edc688cc73675aad4edfa68
Khoa <dangkhoanguyen0812@gmail.com>
2025-03-26 22:58:13 +0700

    finish landing_page_real_estate

--- my-app/src/pages/LandingPage_RealEstate.tsx
+++ my-app/src/pages/LandingPage_RealEstate.tsx
@@ -1,11 +1,23 @@
-import { Contact } from "../components/software/Contact";
-import { Footer } from "../components/software/Footer";
-import Navbar from "../components/software/Navbar";
+import { About } from "../components/real_estate/About";
+import { Brands } from "../components/real_estate/Brands";
+import { Contact } from "../components/real_estate/Contact";
+import { Features } from "../components/real_estate/Features";
+import { Footer } from "../components/real_estate/Footer";
+import { Hero } from "../components/real_estate/Hero";
+import { PropertiesList } from "../components/real_estate/PropertiesList";
+import Team from "../components/real_estate/Team";
+import { Testimonial } from "../components/real_estate/Testimonial";
 
 export const LandingPage_RealEstate = () => {
   return (
     <div>
-      <Navbar />
+      <Hero />
+      <Features />
+      <About />
+      <PropertiesList />
+      <Brands />
+      <Testimonial />
+      <Team />
       <Contact />
       <Footer />
     </div>
