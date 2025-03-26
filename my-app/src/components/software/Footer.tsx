f3f6eefe96e1f4e0cc7f78fa792aa3978612fd7f
Khoa <dangkhoanguyen0812@gmail.com>
2025-03-26 14:55:34 +0700

    add Footer

---
+++ my-app/src/components/software/Footer.tsx
@@ -0,0 +1,29 @@
+export const Footer = () => {
+  return (
+    <div className="flex items-center justify-between px-[250px] py-[30px] bg-[#F9F9F9] absolute bottom-0 w-full ">
+      <p>Copyright © 2024. JustHome</p>
+      <img
+        src="/logo_real_estate_dark.svg"
+        alt="logo"
+        width={200}
+        height={57}
+      />
+      <div className="flex gap-[12px]">
+        <img src="/real_estate/facebook.svg" alt="fb" width={16} height={16} />
+        <img
+          src="/real_estate/twitter.svg"
+          alt="twitter"
+          width={16}
+          height={16}
+        />
+        <img src="/real_estate/ig.svg" alt="ig" width={16} height={16} />
+        <img
+          src="/real_estate/linkedin.svg"
+          alt="linkedin"
+          width={16}
+          height={16}
+        />
+      </div>
+    </div>
+  );
+};
