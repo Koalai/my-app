858778d5d3ea7d6477a890406d1e5a87ee7dca97
Khoa <dangkhoanguyen0812@gmail.com>
2025-04-05 12:34:18 +0700

    add brands

---
+++ my-app/src/components/ecommerce_2/Brands.tsx
@@ -0,0 +1,22 @@
+const logos = [
+  { src: "/ecommerce/versace.png", alt: "versace logo" },
+  { src: "/ecommerce/prada.png", alt: "versace logo" },
+  { src: "/ecommerce/gucci.png", alt: "versace logo" },
+  { src: "/ecommerce/CK.png", alt: "versace logo" },
+  { src: "/ecommerce/zara.png", alt: "versace logo" },
+];
+
+export const Brands = () => {
+  return (
+    <div className="px-[60px] py-11 flex  bg-black max-w-screen-2xl overflow-x-auto justify-center gap-8">
+      {logos.map((logo, index) => (
+        <img
+          key={index}
+          src={logo.src}
+          alt={logo.alt}
+          className="w-1/6 h-[36px] object-contain"
+        />
+      ))}
+    </div>
+  );
+};
