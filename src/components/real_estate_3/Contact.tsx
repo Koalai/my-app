8cacc795c729933a61a5ed8741b083c740bade99
Khoa <dangkhoanguyen0812@gmail.com>
2025-04-08 22:16:20 +0700

    add contact

---
+++ src/components/real_estate_3/Contact.tsx
@@ -0,0 +1,112 @@
+export const Contact = () => {
+  return (
+    <div className="py-[60px] bg-black mx-6 rounded-lg">
+      <div className="flex gap-[100px]  text-white max-w-[1360px] mx-auto w-full">
+        <div className="w-1/2">
+          <div className="bg-[#28282C] px-3  rounded-[50px] w-[90px]">
+            Contact
+          </div>
+          <p className="text-[50px]">Get in touch</p>
+          <p className="mt-5 text-[22px] text-[#D0D1DB]">
+            For any inquiries or to explore your vision further, we invite you
+            to contact our professional team using the details provided below.
+          </p>
+
+          <p className=" w-full text-[18px] font-medium mt-10">
+            Office
+            <span className="float-right font-normal">
+              150 Old Park Ln, London W1K 1QZ
+            </span>
+          </p>
+          <p className=" w-full text-[18px] font-medium mt-5">
+            Email
+            <span className="float-right font-normal">hello@refit.com</span>
+          </p>
+          <p className=" w-full text-[18px] font-medium mt-5 border-b-[1px] pb-[30px] border-white border-opacity-20">
+            Telephone
+            <span className="float-right font-normal">07716 534984</span>
+          </p>
+
+          <p className=" text-[18px] font-medium mt-10">Follow us</p>
+          <div className="flex gap-2 h-6 mt-4">
+            <svg
+              fill="#fff"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path d="M7.03.084c-1.277.06-2.149.264-2.91.563a5.874 5.874 0 0 0-2.124 1.388 5.878 5.878 0 0 0-1.38 2.127C.321 4.926.12 5.8.064 7.076.008 8.354-.005 8.764.001 12.023c.007 3.259.021 3.667.083 4.947.061 1.277.264 2.149.563 2.911.308.789.72 1.457 1.388 2.123a5.872 5.872 0 0 0 2.129 1.38c.763.295 1.636.496 2.913.552 1.278.056 1.689.069 4.947.063 3.257-.007 3.668-.021 4.947-.082 1.28-.06 2.147-.265 2.91-.563a5.881 5.881 0 0 0 2.123-1.388 5.881 5.881 0 0 0 1.38-2.129c.295-.763.496-1.636.551-2.912.056-1.28.07-1.69.063-4.948-.006-3.258-.02-3.667-.081-4.947-.06-1.28-.264-2.148-.564-2.911a5.892 5.892 0 0 0-1.387-2.123 5.857 5.857 0 0 0-2.128-1.38c-.764-.294-1.636-.496-2.914-.55C15.647.009 15.236-.006 11.977 0 8.718.008 8.31.021 7.03.084m.14 21.693c-1.17-.05-1.805-.245-2.228-.408a3.736 3.736 0 0 1-1.382-.895 3.695 3.695 0 0 1-.9-1.378c-.165-.423-.363-1.058-.417-2.228-.06-1.264-.072-1.644-.08-4.848-.006-3.204.006-3.583.061-4.848.05-1.169.246-1.805.408-2.228.216-.561.477-.96.895-1.382a3.705 3.705 0 0 1 1.379-.9c.423-.165 1.057-.361 2.227-.417 1.265-.06 1.644-.072 4.848-.08 3.203-.006 3.583.006 4.85.062 1.168.05 1.804.244 2.227.408.56.216.96.475 1.382.895.421.42.681.817.9 1.378.165.422.362 1.056.417 2.227.06 1.265.074 1.645.08 4.848.005 3.203-.006 3.583-.061 4.848-.051 1.17-.245 1.805-.408 2.23-.216.56-.477.96-.896 1.38a3.705 3.705 0 0 1-1.378.9c-.422.165-1.058.362-2.226.418-1.266.06-1.645.072-4.85.079-3.204.007-3.582-.006-4.848-.06m9.783-16.192a1.44 1.44 0 1 0 1.437-1.442 1.44 1.44 0 0 0-1.437 1.442M5.839 12.012a6.161 6.161 0 1 0 12.323-.024 6.162 6.162 0 0 0-12.323.024M8 12.008A4 4 0 1 1 12.008 16 4 4 0 0 1 8 12.008" />
+            </svg>
+            <svg
+              fill="#fff"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path d="M12.525.02c1.31-.02 2.61-.01 3.91-.02.08 1.53.63 3.09 1.75 4.17 1.12 1.11 2.7 1.62 4.24 1.79v4.03c-1.44-.05-2.89-.35-4.2-.97-.57-.26-1.1-.59-1.62-.93-.01 2.92.01 5.84-.02 8.75-.08 1.4-.54 2.79-1.35 3.94-1.31 1.92-3.58 3.17-5.91 3.21-1.43.08-2.86-.31-4.08-1.03-2.02-1.19-3.44-3.37-3.65-5.71-.02-.5-.03-1-.01-1.49.18-1.9 1.12-3.72 2.58-4.96 1.66-1.44 3.98-2.13 6.15-1.72.02 1.48-.04 2.96-.04 4.44-.99-.32-2.15-.23-3.02.37-.63.41-1.11 1.04-1.36 1.75-.21.51-.15 1.07-.14 1.61.24 1.64 1.82 3.02 3.5 2.87 1.12-.01 2.19-.66 2.77-1.61.19-.33.4-.67.41-1.06.1-1.79.06-3.57.07-5.36.01-4.03-.01-8.05.02-12.07z" />
+            </svg>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+              <path
+                fill="none"
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="1.5"
+                d="m3 21 7.548-7.548M21 3l-7.548 7.548m0 0L8 3H3l7.548 10.452m2.904-2.904L21 21h-5l-5.452-7.548"
+                color="currentColor"
+              />
+            </svg>
+          </div>
+        </div>
+        <form
+          action=""
+          className="p-5 bg-white text-black flex-1 flex flex-col gap-5 rounded-lg"
+        >
+          <div className="flex flex-col gap-[10px]">
+            <label htmlFor="name">
+              Name<span className="text-red-500">*</span>
+            </label>
+            <input
+              type="text"
+              placeholder="John Smith"
+              className="border-[1px] py-4 px-3 rounded-lg bg-gray-50"
+            />
+          </div>
+          <div className="flex flex-col gap-[10px]">
+            <label htmlFor="name">
+              Email<span className="text-red-500">*</span>
+            </label>
+            <input
+              type="text"
+              placeholder="johnsmith@gmail.com"
+              className="border-[1px] py-4 px-3 rounded-lg bg-gray-50"
+            />
+          </div>
+          <div className="flex flex-col gap-[10px]">
+            <label htmlFor="name">
+              Name<span className="text-red-500">*</span>
+            </label>
+            <input
+              type="text"
+              placeholder="+44789 123456"
+              className="border-[1px] py-4 px-3 rounded-lg bg-gray-50"
+            />
+          </div>
+          <div className="flex flex-col gap-[10px]">
+            <label htmlFor="name">
+              Name<span className="text-red-500">*</span>
+            </label>
+            <textarea
+              placeholder="Hello, I'd like to enquire about..."
+              className="w-full max-w-[600px] h-[120px] p-4 bg-gray-50 rounded-lg border border-gray-200 text-gray-500 text-lg resize-none focus:outline-none focus:ring-2 focus:ring-gray-300"
+            />
+          </div>
+          <button
+            type="submit"
+            className="bg-[#101014BF] px-[18px] py-3 text-white rounded-lg font-medium"
+          >
+            Send message
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
