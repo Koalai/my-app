1218ea9bdd48758a0bd877f60ffd350ef68db317
Khoa <dangkhoanguyen0812@gmail.com>
2025-04-10 21:00:07 +0700

    add why us

---
+++ src/components/real_estate_5/WhyUs.tsx
@@ -0,0 +1,93 @@
+export const WhyUs = () => {
+  return (
+    <div className="pt-[130px] px-5">
+      <div
+        className="max-w-[1370px] mx-auto bg-center bg-cover bg-no-repeat rounded-[32px] h-[700px] relative"
+        style={{ backgroundImage: `url('/real_estate_5/whyUs.png')` }}
+      >
+        <button className="flex rounded-[50px] bg-white h-[52px] items-center justify-center gap-[30px] pl-4 py-[2px] pr-[2px] text-xl font-medium absolute top-12 left-20">
+          Top Experience
+          <span className="rounded-full border-[1px] w-[48px] h-[48px] flex justify-center items-center -rotate-[24deg] border-black">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              fill="#000000"
+              viewBox="0 0 256 256"
+            >
+              <path d="M221.66,133.66l-72,72a8,8,0,0,1-11.32-11.32L196.69,136H40a8,8,0,0,1,0-16H196.69L138.34,61.66a8,8,0,0,1,11.32-11.32l72,72A8,8,0,0,1,221.66,133.66Z"></path>
+            </svg>
+          </span>
+        </button>
+        <button className="flex rounded-[50px] bg-white h-[52px] items-center justify-center gap-[30px] pl-4 py-[2px] pr-[2px] text-xl font-medium absolute top-20 right-48">
+          24/7 Support
+          <span className="rounded-full border-[1px] w-[48px] h-[48px] flex justify-center items-center -rotate-[24deg] border-black">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              fill="#000000"
+              viewBox="0 0 256 256"
+            >
+              <path d="M221.66,133.66l-72,72a8,8,0,0,1-11.32-11.32L196.69,136H40a8,8,0,0,1,0-16H196.69L138.34,61.66a8,8,0,0,1,11.32-11.32l72,72A8,8,0,0,1,221.66,133.66Z"></path>
+            </svg>
+          </span>
+        </button>
+        <button className="flex rounded-[50px] bg-white h-[52px] items-center justify-center gap-[30px] pl-4 py-[2px] pr-[2px] text-xl font-medium absolute bottom-12 right-20">
+          Transparent Reporting
+          <span className="rounded-full border-[1px] w-[48px] h-[48px] flex justify-center items-center -rotate-[24deg] border-black">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              fill="#000000"
+              viewBox="0 0 256 256"
+            >
+              <path d="M221.66,133.66l-72,72a8,8,0,0,1-11.32-11.32L196.69,136H40a8,8,0,0,1,0-16H196.69L138.34,61.66a8,8,0,0,1,11.32-11.32l72,72A8,8,0,0,1,221.66,133.66Z"></path>
+            </svg>
+          </span>
+        </button>
+        <button className="flex rounded-[50px] bg-white h-[52px] items-center justify-center gap-[30px] pl-4 py-[2px] pr-[2px] text-xl font-medium absolute bottom-40 right-96">
+          Transparent Reporting
+          <span className="rounded-full border-[1px] w-[48px] h-[48px] flex justify-center items-center -rotate-[24deg] border-black">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              fill="#000000"
+              viewBox="0 0 256 256"
+            >
+              <path d="M221.66,133.66l-72,72a8,8,0,0,1-11.32-11.32L196.69,136H40a8,8,0,0,1,0-16H196.69L138.34,61.66a8,8,0,0,1,11.32-11.32l72,72A8,8,0,0,1,221.66,133.66Z"></path>
+            </svg>
+          </span>
+        </button>
+        <div className="rounded-[32px] p-[32px] flex flex-col gap-5 absolute bottom-4 left-4 bg-white w-[600px]">
+          <div className="py-2 px-5 border-[1px] border-black rounded-[32px] font-medium text-lg w-1/3">
+            Why choose us
+          </div>
+          <div className="flex gap-[30px] flex-col">
+            <p className="text-[50px] font-semibold leading-tight">
+              Discover why we're your top choice for real estate services.
+            </p>
+            <div className="flex">
+              <button className="py-[18px] px-10 bg-[#CBB3FF] rounded-[50px] text-xl h-[54px] leading-none font-normal">
+                Get Started
+              </button>
+              <span className="rounded-full bg-[#CBB3FF] w-[54px] h-[54px] flex justify-center items-center -rotate-[24deg]">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  fill="#000000"
+                  viewBox="0 0 256 256"
+                >
+                  <path d="M221.66,133.66l-72,72a8,8,0,0,1-11.32-11.32L196.69,136H40a8,8,0,0,1,0-16H196.69L138.34,61.66a8,8,0,0,1,11.32-11.32l72,72A8,8,0,0,1,221.66,133.66Z"></path>
+                </svg>
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
