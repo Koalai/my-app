825d2827a89fc3135019683acb7adbd5687ae3a1
Khoa <dangkhoanguyen0812@gmail.com>
2025-04-15 10:37:15 +0700

    add message and services

---
+++ src/components/law/Message.tsx
@@ -0,0 +1,19 @@
+export const Message = () => {
+  return (
+    <div className="grid grid-cols-2">
+      <img src="/law/message_bg.png" alt="banner" />
+      <div className="py-[160px] px-12 flex flex-col border-b-[1px] justify-center items-start gap-8 ">
+        <p className="text-[28px]">
+          With the inspiring power of law, We build the future: Welcome to our
+          law firm! Proudly leading with a dedicated and passionate team, we
+          pursue justice. Making a difference in Legal Solutions with
+          avant-garde approaches!
+        </p>
+        <div>
+          <p className="text-[19px]">Ceren Kurt</p>
+          <p className="text-[#830000] pt-2">Commercial Lawyer</p>
+        </div>
+      </div>
+    </div>
+  );
+};
---
+++ src/components/law/Services.tsx
@@ -0,0 +1,68 @@
+const data = [
+  {
+    title: "Commercial Law",
+    descriptions:
+      "Commercial Law is the branch of law that regulates the establishment, management, and commercial transactions of businesses. It plays a crucial role in resolving disputes and ensuring legal compliance in commercial dealings.",
+  },
+  {
+    title: "Intellectual Property",
+    descriptions:
+      "Intellectual Property (IP) refers to creations of the mind, such as inventions, art, and trademarks, protected by law to promote innovation and creativity.",
+  },
+  {
+    title: "Employment Law",
+    descriptions:
+      "Employment Law, the area of law that governs the legal relationship between employers and employees. It encompasses a wide range of issues related to the workplace.",
+  },
+  {
+    title: "Real Estate Law",
+    descriptions:
+      "Real Estate Law is the legal domain that deals with the legal aspects of immovable properties, such as houses, lands, buildings, etc.",
+  },
+  {
+    title: "General Contracts",
+    descriptions:
+      "General Contracts are legally binding agreements between parties, covering various everyday situations like buying, selling, services, leasing, or lending.",
+  },
+  {
+    title: "Tax Law",
+    descriptions:
+      "Tax Law, the legal field that deals with the rules, regulations, and policies governing taxation. It encompasses various tax-related matters, such as income tax, corporate tax, property tax, sales tax, and international taxation.",
+  },
+];
+
+export const Services = () => {
+  return (
+    <div className="py-[160px] px-12 flex flex-col gap-10">
+      <div className="flex flex-col gap-3">
+        <p className="text-[19px] text-[#830000]">Explore All Practice Areas</p>
+        <p className="text-[40px]">Our Specialisations</p>
+      </div>
+      <div className="grid grid-cols-3 grid-rows-2 gap-16">
+        {data.map((item, index) => (
+          <div
+            key={index}
+            className="border-r-[1px] py-8 pr-16 relative group overflow-hidden"
+          >
+            <div className="flex flex-col gap-4 w-4/5 z-10 relative">
+              <span className="text-[19px] text-[#830000]">/0{index + 1}</span>
+              <p className="text-[33px]">{item.title}</p>
+              <p className="text-[#696969]">{item.descriptions}</p>
+            </div>
+            <div className="bg-[#830000] w-[80px] absolute right-0 h-full top-0 flex justify-center items-center transform translate-x-full group-hover:translate-x-0 transition-transform duration-300 ease-in-out">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                fill="#ffffff"
+                viewBox="0 0 256 256"
+              >
+                <path d="M221.66,133.66l-72,72a8,8,0,0,1-11.32-11.32L196.69,136H40a8,8,0,0,1,0-16H196.69L138.34,61.66a8,8,0,0,1,11.32-11.32l72,72A8,8,0,0,1,221.66,133.66Z"></path>
+              </svg>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
